49192de54064c5350b973446fc4c26dbacb35472
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-03-08 14:00:46 +0900

    updated rename chunks

--- rename-chunks.sh
+++ rename-chunks.sh
@@ -2,6 +2,6 @@
 ORIGIN=build/static
 
 cp $ORIGIN/js/main*.js $ORIGIN/js/main.js
-cp $ORIGIN/949*.js $ORIGIN/js/949.js
-cp $ORIGIN/787*.js $ORIGIN/js/787.js
+cp $ORIGIN/js/949*.js $ORIGIN/js/949.js
+cp $ORIGIN/js/787*.js $ORIGIN/js/787.js
 cp $ORIGIN/css/main*.css $ORIGIN/css/main.css
